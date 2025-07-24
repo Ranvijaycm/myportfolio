74520ac45cfa619210c324976ba8c2075ac4abee
Ranvijaycm <rnvjtest1@gmail.com>
2025-07-24 22:04:08 +0530

    adding the portfolio html and css files

---
+++ dumdum/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("themeToggle").addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+//   const mode = document.body.classList.contains("dark") ? "Dark" : "Light";
+//   document.getElementById("themeToggle").textContent = mode;
+// });
